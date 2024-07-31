fcc56d00f800f12d9f57d3dab49638ffbba3627f
Theo <73323028+kkwondev@users.noreply.github.com>
2024-07-31 03:31:11 +0000

    errorPlugin modify

--- src/plugins/errorPlugin.ts
+++ src/plugins/errorPlugin.ts
@@ -5,28 +5,16 @@ import { isHttpError } from '../tools/errors/httpError';
 
 const errorPlugin: FastifyPluginCallback = (fastify, _, done) => {
   fastify.setErrorHandler((error, request, reply) => {
-    console.log(error);
-    if (isHttpError(error)) {
-      reply.status(error.statusCode).send({
-        result: false,
-        data: {
-          statusCode: error.statusCode,
-          name: error.name,
-          message: error.message,
-        },
-      });
-    } else {
-      const statusCode = error.statusCode || 500;
-      reply.status(statusCode).send({
-        result: false,
-        error: {
-          code: error.code || 'INTERNAL_SERVER_ERROR',
-          message: error.message || 'Internal Server Error',
-          name: error.name || 'Error',
-          statusCode,
-        },
-      });
-    }
+    const statusCode = error.statusCode || 500;
+    reply.status(statusCode).send({
+      result: false,
+      error: {
+        code: error.code || 'INTERNAL_SERVER_ERROR',
+        message: error.message || 'Internal Server Error',
+        name: error.name || 'Error',
+        statusCode,
+      },
+    });
     done();
   });
 
